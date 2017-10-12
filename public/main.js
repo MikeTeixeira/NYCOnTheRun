d866f252465e7a4d1220794310f7ee3fd1cd81d3
christykusuma <christykusuma96@gmail.com>
2017-10-12 16:52:07 -0400

    with modal working

--- public/main.js
+++ public/main.js
@@ -3,7 +3,8 @@ document.addEventListener("DOMContentLoaded", function(){
   let elModal = document.querySelector(".modal");
   let elRaceButton = document.querySelector("#race_button");
 
-	  elRaceButton.addEventListener("click", function(){
+	elRaceButton.addEventListener("click", function(){
     elModal.classList.add("ease_in");
+      });
   });
 

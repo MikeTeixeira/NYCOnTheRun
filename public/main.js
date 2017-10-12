87ac30da26d3d444f4c14b097853d83558d852ba
christykusuma <christykusuma96@gmail.com>
2017-10-12 10:53:11 -0400

    final final copy

--- public/main.js
+++ public/main.js
@@ -3,10 +3,8 @@ document.addEventListener("DOMContentLoaded", function(){
   let elModal = document.querySelector(".modal");
   let elRaceButton = document.querySelector("#race_button");
 
-  if( elRaceButton ) {
 	  elRaceButton.addEventListener("click", function(){
     elModal.classList.add("ease_in");
       });
   });
-});
 
